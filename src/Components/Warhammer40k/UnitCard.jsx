c01124b5aa3f76ba4cf92c663b2213946d797b01
Ron Planken <ron.planken@gmail.com>
2022-08-13 13:26:54 +0200

    Added optional background to unit cards

--- src/Components/Warhammer40k/UnitCard.jsx
+++ src/Components/Warhammer40k/UnitCard.jsx
@@ -14,127 +14,133 @@ export const UnitCard = ({ unit, cardStyle, paddingTop = "32px" }) => {
       }}>
       <div className={`page ${unit.variant || "card"}`} style={cardStyle}>
         <div className="frame">
-          <div className="header">
-            <div className="role">
-              <UnitType type={unit.role} />
+          <div className={unit.background || "NONE"}>
+            <div className="header">
+              <div className="role">
+                <UnitType type={unit.role} />
+              </div>
+              <div className="name">{unit.name}</div>
             </div>
-            <div className="name">{unit.name}</div>
-          </div>
-          {unit.datasheet?.filter((sheet) => sheet.active).length > 0 && (
-            <div className="labels heading">
-              <div className="center label">
-                <div className="movement icon" title="Movement" alt-text="Movement"></div>
+            {unit.datasheet?.filter((sheet) => sheet.active).length > 0 && (
+              <div className="labels heading">
+                <div className="center label">
+                  <div className="movement icon" title="Movement" alt-text="Movement"></div>
+                </div>
+                <div className="center label">
+                  <div className="weaponskill icon" title="Weapon Skill" alt-text="Weapon Skill"></div>
+                </div>
+                <div className="center label">
+                  <div className="ballisticskill icon" title="Ballistic Skill" alt-text="Ballistic Skill"></div>
+                </div>
+                <div className="center label">
+                  <div className="strength icon" title="Strength" alt-text="Strength"></div>
+                </div>
+                <div className="center label">
+                  <div className="toughness icon" title="Toughness" alt-text="Toughness"></div>
+                </div>
+                <div className="center label">
+                  <div className="wounds icon" title="Wounds" alt-text="Wounds"></div>
+                </div>
+                <div className="center label">
+                  <div className="attacks icon" title="Attacks" alt-text="Attacks"></div>
+                </div>
+                <div className="center label">
+                  <div className="leadership icon" title="Leadership" alt-text="Leadership"></div>
+                </div>
+                <div className="center label">
+                  <div className="save icon" title="Save" alt-text="Save"></div>
+                </div>
+                <div className="center label">
+                  <div className="inv icon" title="Invulnerable" alt-text="Save"></div>
+                </div>
               </div>
-              <div className="center label">
-                <div className="weaponskill icon" title="Weapon Skill" alt-text="Weapon Skill"></div>
-              </div>
-              <div className="center label">
-                <div className="ballisticskill icon" title="Ballistic Skill" alt-text="Ballistic Skill"></div>
-              </div>
-              <div className="center label">
-                <div className="strength icon" title="Strength" alt-text="Strength"></div>
-              </div>
-              <div className="center label">
-                <div className="toughness icon" title="Toughness" alt-text="Toughness"></div>
-              </div>
-              <div className="center label">
-                <div className="wounds icon" title="Wounds" alt-text="Wounds"></div>
-              </div>
-              <div className="center label">
-                <div className="attacks icon" title="Attacks" alt-text="Attacks"></div>
-              </div>
-              <div className="center label">
-                <div className="leadership icon" title="Leadership" alt-text="Leadership"></div>
-              </div>
-              <div className="center label">
-                <div className="save icon" title="Save" alt-text="Save"></div>
-              </div>
-              <div className="center label">
-                <div className="inv icon" title="Invulnerable" alt-text="Save"></div>
-              </div>
+            )}
+            <div className="profile">
+              {unit.datasheet?.map((datasheet, index) => {
+                if (datasheet.active) {
+                  return <UnitStatline statline={datasheet} key={`datasheet-${index}`} />;
+                } else {
+                  return <></>;
+                }
+              })}
             </div>
-          )}
-          <div className="profile">
-            {unit.datasheet?.map((datasheet, index) => {
-              if (datasheet.active) {
-                return <UnitStatline statline={datasheet} key={`datasheet-${index}`} />;
-              } else {
-                return <></>;
-              }
-            })}
-          </div>
-          {unit.unit_composition_active && (
-            <div className="description">
-              <MarkdownDisplay content={unit.unit_composition} />
-            </div>
-          )}
-          {unit.wargear?.filter((sheet) => sheet.active).length > 0 && (
-            <div className="weapons heading">
-              <div className="left label">WEAPON</div>
-              <div className="center label">
-                <div className="range icon" title="Range" alt-text="Range"></div>
+            {unit.unit_composition_active && (
+              <div className="description">
+                <MarkdownDisplay content={unit.unit_composition} />
               </div>
-              <div className="center label">
-                <div className="type icon" title="Type" alt-text="Type"></div>
+            )}
+            {unit.wargear?.filter((sheet) => sheet.active).length > 0 && (
+              <div className="weapons heading">
+                <div className="left label">WEAPON</div>
+                <div className="center label">
+                  <div className="range icon" title="Range" alt-text="Range"></div>
+                </div>
+                <div className="center label">
+                  <div className="type icon" title="Type" alt-text="Type"></div>
+                </div>
+                <div className="center label">
+                  <div className="strength icon" title="Type" alt-text="Type"></div>
+                </div>
+                <div className="center label">
+                  <div className="ap icon" title="Type" alt-text="Type"></div>
+                </div>
+                <div className="center label">
+                  <div className="dmg icon" title="Type" alt-text="Type"></div>
+                </div>
               </div>
-              <div className="center label">
-                <div className="strength icon" title="Type" alt-text="Type"></div>
-              </div>
-              <div className="center label">
-                <div className="ap icon" title="Type" alt-text="Type"></div>
-              </div>
-              <div className="center label">
-                <div className="dmg icon" title="Type" alt-text="Type"></div>
-              </div>
-            </div>
-          )}
-          <div className="profile">
-            {unit.wargear?.map((wargear, index) => {
-              if (!wargear.active) {
-                return <></>;
-              }
-              if (wargear.profiles.length > 1) {
+            )}
+            <div className="profile">
+              {unit.wargear?.map((wargear, index) => {
+                if (!wargear.active) {
+                  return <></>;
+                }
+                if (wargear.profiles.length > 1) {
+                  return (
+                    <>
+                      <div className="description" key={`profile-${index}-description`}>
+                        {wargear.name}
+                      </div>
+                      {wargear.profiles.map((profile, pindex) => {
+                        return (
+                          <WeaponStatline
+                            profile={profile}
+                            key={`profile-${index}-${pindex}`}
+                            type={unit.variant || "card"}
+                          />
+                        );
+                      })}
+                    </>
+                  );
+                }
                 return (
-                  <>
-                    <div className="description" key={`profile-${index}-description`}>
-                      {wargear.name}
-                    </div>
-                    {wargear.profiles.map((profile, pindex) => {
-                      return (
-                        <WeaponStatline
-                          profile={profile}
-                          key={`profile-${index}-${pindex}`}
-                          type={unit.variant || "card"}
-                        />
-                      );
-                    })}
-                  </>
+                  <WeaponStatline
+                    profile={wargear.profiles[0]}
+                    key={`profile-${index}`}
+                    type={unit.variant || "card"}
+                  />
                 );
-              }
-              return (
-                <WeaponStatline profile={wargear.profiles[0]} key={`profile-${index}`} type={unit.variant || "card"} />
-              );
-            })}
-          </div>
-          <div className="abilities">
-            {unit.abilities?.map((ability, index) => {
-              return (
-                ability.showAbility && (
-                  <div className="description" key={`ability-${ability.name}-description-${index}`}>
-                    {!ability.showDescription && <b>{ability.name}</b>}
-                    {ability.showDescription && (
-                      <MarkdownDisplay content={`**${ability.name}** : ${ability.description}`} />
-                    )}
-                  </div>
-                )
-              );
-            })}
-          </div>
-          <div className="footer">
-            {unit.keywords
-              ?.filter((keyword) => keyword.active)
-              .map((keyword) => keyword.keyword)
-              .join(", ")}
+              })}
+            </div>
+            <div className="abilities">
+              {unit.abilities?.map((ability, index) => {
+                return (
+                  ability.showAbility && (
+                    <div className="description" key={`ability-${ability.name}-description-${index}`}>
+                      {!ability.showDescription && <b>{ability.name}</b>}
+                      {ability.showDescription && (
+                        <MarkdownDisplay content={`**${ability.name}** : ${ability.description}`} />
+                      )}
+                    </div>
+                  )
+                );
+              })}
+            </div>
+            <div className="footer">
+              {unit.keywords
+                ?.filter((keyword) => keyword.active)
+                .map((keyword) => keyword.keyword)
+                .join(", ")}
+            </div>
           </div>
         </div>
       </div>
--- src/Components/Warhammer40k/UnitCardEditor/UnitBasicInfo.jsx
+++ src/Components/Warhammer40k/UnitCardEditor/UnitBasicInfo.jsx
@@ -37,6 +37,7 @@ export function UnitBasicInfo() {
           onChange={(value) => updateActiveCard({ ...activeCard, role: value })}
         >
           <Option value="HQ">HQ</Option>
+          <Option value="Troops">Troops</Option>
           <Option value="Elites">Elites</Option>
           <Option value="Heavy Support">Heavy Support</Option>
           <Option value="Fast Attack">Fast Attack</Option>
@@ -45,6 +46,54 @@ export function UnitBasicInfo() {
           <Option value="Fortifications">Fortifications</Option>
           <Option value="Lords of War">Lords of War</Option>
           <Option value="Unknown">Unknown</Option>
+        </Select>
+      </Form.Item>
+      <Form.Item label={"Background"}>
+        <Select
+          value={activeCard.background || "NONE"}
+          onChange={(value) => updateActiveCard({ ...activeCard, background: value })}>
+          <Option value="NONE">None</Option>
+          <Option value="AC">Adeptus Custodes</Option>
+          <Option value="AE">Asuryani</Option>
+          <Option value="AL">Alpha Legion</Option>
+          <Option value="AM">Astra Militarum</Option>
+          <Option value="AS">Adepta Sororitas</Option>
+          <Option value="AdM">Adeptus Mechanicus</Option>
+          <Option value="BA">Blood Angels</Option>
+          <Option value="BL">Black Legion</Option>
+          <Option value="BT">Black Templars</Option>
+          <Option value="CD">Chaos Demons</Option>
+          <Option value="CSM">Chaos Space Marines</Option>
+          <Option value="CoB">Creations of Bile</Option>
+          <Option value="DA">Dark Angels</Option>
+          <Option value="DG">Death Guard</Option>
+          <Option value="DRU">Drukhari</Option>
+          <Option value="DW">Deathwatch</Option>
+          <Option value="EC">Emperor&apos;s Children</Option>
+          <Option value="GC">Genestealer Cult</Option>
+          <Option value="GK">Grey Knights</Option>
+          <Option value="HAR">Harlequins</Option>
+          <Option value="IF">Imperial Fists</Option>
+          <Option value="IH">Iron Hands</Option>
+          <Option value="IW">Iron Warriors</Option>
+          <Option value="NEC">Necrons</Option>
+          <Option value="NL">Night Lords</Option>
+          <Option value="ORK">Orks</Option>
+          <Option value="QI">Imperial Knights</Option>
+          <Option value="QT">Chaos Knights</Option>
+          <Option value="RC">Red Corsairs</Option>
+          <Option value="RG">Raven Guard</Option>
+          <Option value="SA">Salamanders</Option>
+          <Option value="SM">Space Marines</Option>
+          <Option value="SW">Space Wolves</Option>
+          <Option value="TAU">Tau</Option>
+          <Option value="TS">Thousand Sons</Option>
+          <Option value="TYR">Tyranids</Option>
+          <Option value="UL">Ultramarines</Option>
+          <Option value="WB">Word Bearers</Option>
+          <Option value="WE">World Eaters</Option>
+          <Option value="WS">White Scars</Option>
+          <Option value="basic">Basic</Option>
         </Select>
       </Form.Item>
     </>
