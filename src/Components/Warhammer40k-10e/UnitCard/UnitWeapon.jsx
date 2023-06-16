ddcf95b045dd5983cf09b5f0b6029d00992193f1
Ron Planken <ron.planken@gmail.com>
2023-06-16 13:13:48 +0200

    Fixed empty lines still showing some border art when disabled

--- src/Components/Warhammer40k-10e/UnitCard/UnitWeapon.jsx
+++ src/Components/Warhammer40k-10e/UnitCard/UnitWeapon.jsx
@@ -2,23 +2,25 @@ import { UnitWeaponKeywords } from "./UnitWeaponKeyword";
 
 export const UnitWeapon = ({ weapon }) => {
   return (
-    <div className="weapon">
+    <>
       {weapon.profiles
         ?.filter((line) => line.active)
         ?.map((line, index) => (
-          <div className="line" key={`weapon-line-${index}`}>
-            <div className="value">
-              {line.name}
-              {line.keywords?.length > 0 && <UnitWeaponKeywords keywords={line.keywords} />}
+          <div className="weapon" key={`weapon-line-${index}`}>
+            <div className="line">
+              <div className="value">
+                {line.name}
+                {line.keywords?.length > 0 && <UnitWeaponKeywords keywords={line.keywords} />}
+              </div>
+              <div className="value center">{line.range}</div>
+              <div className="value center">{line.attacks}</div>
+              <div className="value center">{line.skill}</div>
+              <div className="value center">{line.strength}</div>
+              <div className="value center">{line.ap}</div>
+              <div className="value center">{line.damage}</div>
             </div>
-            <div className="value center">{line.range}</div>
-            <div className="value center">{line.attacks}</div>
-            <div className="value center">{line.skill}</div>
-            <div className="value center">{line.strength}</div>
-            <div className="value center">{line.ap}</div>
-            <div className="value center">{line.damage}</div>
           </div>
         ))}
-    </div>
+    </>
   );
 };
