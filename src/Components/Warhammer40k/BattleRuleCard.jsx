fde856f79fc2d3966019bfb349b579fec7e251b3
rabidcicada <rabidcicada@gmail.com>
2023-08-31 19:03:35 -0400

    Adapt to use secondary styling that is already in the css

--- src/Components/Warhammer40k/BattleRuleCard.jsx
+++ src/Components/Warhammer40k/BattleRuleCard.jsx
@@ -2,6 +2,8 @@ import { ReactFitty } from "react-fitty";
 import { MarkdownSpanDisplay } from "../MarkdownSpanDisplay";
 
 export const BattleRuleCard = ({ battle_rule, cardStyle, paddingTop = "32px", className }) => {
+  const style =
+    battle_rule.variant === "custom" ? { height: `${battle_rule.height}cm`, width: `${battle_rule.width}cm` } : {};
   return (
     <div
       className={className}
@@ -12,51 +14,47 @@ export const BattleRuleCard = ({ battle_rule, cardStyle, paddingTop = "32px", cl
         display: "flex",
         paddingTop: paddingTop,
       }}>
-      <div className={`battle_rule`}>
-        <div className="background-side-bar"></div>
-        <div className="background-header-bar"></div>
-        <div className="header">
-          <ReactFitty maxSize={16} minSize={10}>
-            {battle_rule.name}
-          </ReactFitty>
-        </div>
-        <div className="type">
-          <ReactFitty maxSize={10} minSize={8}>
-            {battle_rule.detachment} - {battle_rule.rule_type}
-          </ReactFitty>
-        </div>
-        <div className="content">
-          {battle_rule.rule_type && (
-            <div className="section">
-              <span className="title">When:</span>
-              <span className="text">
-                <MarkdownSpanDisplay content={battle_rule.rule_type} />
-              </span>
+      <div className={`secondary`} style={cardStyle}>
+        <div className={`page secondary`} style={style}>
+          <div className="frame">
+            <div
+              className={`secondary_icon_${battle_rule.faction_id ? battle_rule.faction_id : "basic"}`}
+              style={{ position: "absolute", width: 35, height: 35, top: 10, right: 12 }}
+            />
+            <div className={`${battle_rule.faction_id ? battle_rule.faction_id : "basic"} background`}>
+              <div className={`battle_rule`}>
+                <div className="header">
+                  <div className="type">
+                    <ReactFitty maxSize={10} minSize={8}>
+                      {battle_rule.rule_type} {battle_rule.rule_subtype && "-" + battle_rule.rule_subtype}
+                    </ReactFitty>
+                  </div>
+                  <div className="name">
+                    <ReactFitty maxSize={16} minSize={10}>
+                      {battle_rule.name}
+                    </ReactFitty>
+                  </div>
+                </div>
+                <div className="content">
+                  {battle_rule.flavor_text && (
+                    <div className="section">
+                      <span className="callout_text">target:</span>
+                      <span className="text">
+                        <MarkdownSpanDisplay content={battle_rule.flavor_text} />
+                      </span>
+                    </div>
+                  )}
+                  {battle_rule.main_desc && (
+                    <div className="section">
+                      <span className="callout_text">effect:</span>
+                      <span className="text">
+                        <MarkdownSpanDisplay content={battle_rule.main_desc} />
+                      </span>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-          )}
-          {battle_rule.flavor_text && (
-            <div className="section">
-              <span className="title">target:</span>
-              <span className="text">
-                <MarkdownSpanDisplay content={battle_rule.flavor_text} />
-              </span>
-            </div>
-          )}
-          {battle_rule.main_desc && (
-            <div className="section">
-              <span className="title">effect:</span>
-              <span className="text">
-                <MarkdownSpanDisplay content={battle_rule.main_desc} />
-              </span>
-            </div>
-          )}
-        </div>
-        <div className="type-container"></div>
-        <div className="cp-container">
-          <div className="value">
-            <ReactFitty maxSize={18} minSize={10}>
-              {battle_rule.cost} CP
-            </ReactFitty>
           </div>
         </div>
       </div>
