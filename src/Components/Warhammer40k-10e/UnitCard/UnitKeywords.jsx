92e5b37c9b8c978e3aa9095c5b92d6ee017eba85
Ron Planken <wiebbe@gmail.com>
2024-03-16 11:07:44 +0100

    If keywords contain an item with : split it into a different section

--- src/Components/Warhammer40k-10e/UnitCard/UnitKeywords.jsx
+++ src/Components/Warhammer40k-10e/UnitCard/UnitKeywords.jsx
@@ -2,7 +2,18 @@ export const UnitKeywords = ({ keywords }) => {
   return (
     <div className="keywords">
       <span className="title">keywords</span>
-      <span className="value">{keywords?.join(", ")}</span>
+      <span className="value">
+        {keywords?.map((keyword) => {
+          if (keyword?.includes(":")) {
+            return (
+              <span key={keyword} style={{ fontWeight: 400, textTransform: "uppercase", fontSize: "0.9rem" }}>
+                {keyword}&nbsp;
+              </span>
+            );
+          }
+          return `${keyword}, `;
+        })}
+      </span>
     </div>
   );
 };
