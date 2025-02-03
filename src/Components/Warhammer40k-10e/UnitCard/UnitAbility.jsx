0c77e2afb90a0ef1a70be8d53be6e83ef9e9872a
Ambre Marques <26360235+ambre-m@users.noreply.github.com>
2025-02-03 11:57:43 +0100

    [40k-10] Added data-name attribute to each ability.

--- src/Components/Warhammer40k-10e/UnitCard/UnitAbility.jsx
+++ src/Components/Warhammer40k-10e/UnitCard/UnitAbility.jsx
@@ -4,7 +4,7 @@ export const UnitAbility = ({ name, value }) => {
   return (
     <>
       {value && (
-        <div className="ability">
+        <div className="ability" data-name={name}>
           <span className="title">{name}</span>
           <span className="value">{replaceKeywords(value)}</span>
         </div>
