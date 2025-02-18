f89f5eb3f9edfb9690e7936b3c1b345ccefd875e
Paul Schoen <paulschoen@rivian.com>
2025-02-18 00:02:05 -0600

    add front and back option for easy folding

--- src/Components/Warhammer40k-10e/UnitCard.jsx
+++ src/Components/Warhammer40k-10e/UnitCard.jsx
@@ -5,18 +5,27 @@ import { UnitCardFront } from "./UnitCardFront";
 import { UnitCardFull } from "./UnitCardFull";
 
 export const UnitCard = ({ unit, cardStyle, paddingTop = "32px", className, side = "front" }) => {
-  const { settings, updateSettings } = useSettingsStorage();
+  const { settings } = useSettingsStorage();
 
-  if (unit.variant === "full" || settings.showCardsAsDoubleSided === true) {
-    return <UnitCardFull unit={unit} cardStyle={cardStyle} paddingTop={paddingTop} className={className} />;
-  }
-  if (unit.variant === "basic") {
-    return <UnitCardBasic unit={unit} cardStyle={cardStyle} paddingTop={paddingTop} className={className} />;
-  }
-  if (side === "front") {
-    return <UnitCardFront unit={unit} cardStyle={cardStyle} paddingTop={paddingTop} className={className} />;
-  }
-  if (side === "back") {
-    return <UnitCardBack unit={unit} cardStyle={cardStyle} paddingTop={paddingTop} className={className} />;
+  const showFullUnitCard = settings.showCardsAsDoubleSided || unit.variant === "full";
+  const cardTypeToDisplay = showFullUnitCard ? "full" : unit.variant === "basic" ? "basic" : side;
+
+  switch (cardTypeToDisplay) {
+    case "full":
+      return <UnitCardFull unit={unit} cardStyle={cardStyle} paddingTop={paddingTop} className={className} />;
+    case "basic":
+      return <UnitCardBasic unit={unit} cardStyle={cardStyle} paddingTop={paddingTop} className={className} />;
+    case "frontAndBack":
+      return (
+        <div style={{ display: "flex" }}>
+          <UnitCardFront unit={unit} cardStyle={cardStyle} paddingTop={paddingTop} className={className} />
+          <UnitCardBack unit={unit} cardStyle={cardStyle} paddingTop={paddingTop} className={className} />
+        </div>
+      );
+    case "back":
+      return <UnitCardBack unit={unit} cardStyle={cardStyle} paddingTop={paddingTop} className={className} />;
+    case "front":
+    default:
+      return <UnitCardFront unit={unit} cardStyle={cardStyle} paddingTop={paddingTop} className={className} />;
   }
 };
